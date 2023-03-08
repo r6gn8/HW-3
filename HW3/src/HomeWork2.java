e0663c6a0440b3bffde0c5dd3e60d88e2b18d4ee
Noof Marwan <111057660+r6gn8@users.noreply.github.com>
2023-03-08 10:52:19 +0300

    Add files via upload

---
+++ HW3/src/HomeWork2.java
@@ -0,0 +1,9 @@
+//Cloning Objects Arrays
+import java.util.Arrays;
+public class HomeWork2 {
+    public static void main(String[] args) {
+        String[] names={"anfal","asmaa"};
+        String[] copy=names;
+        System.out.println(Arrays.toString(copy));
+    }
+}
